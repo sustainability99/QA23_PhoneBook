ccec39c7737b244ef0e7ace672c760fde62b0a04
sustainability99 <126771099+sustainability99@users.noreply.github.com>
2023-06-19 19:46:22 +0200

    add new Test

--- src/test/java/com/phonebook/tests/LoginTests.java
+++ src/test/java/com/phonebook/tests/LoginTests.java
@@ -5,30 +5,25 @@ import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class CreateAccountTests extends TestBase {
+public class LoginTests extends TestBase{
 
-    //precondition: user should be logged out
     @BeforeMethod
     public void ensurePrecondition() {
         if (!isElementPresent(By.xpath("//a[.='LOGIN']"))) {
             click(By.xpath("//button[.='Sign Out']"));
         }
-        //click on Login link
         click(By.xpath("//a[.='LOGIN']"));
     }
 
     @Test
-    public void existedUserRegistrationNegativeTest() {
+    public void loginPositiveTest() {
         //enter email field
         type(By.cssSelector("[placeholder='Email']"), "kan@gmai.com");
         //enter password field
         type(By.cssSelector("[placeholder='Password']"), "Kan123$-_$");
-        //click on Registration
-        click(By.name("registration"));
+        //click on Login button
+        click(By.name("login"));
         //assert user logged in(check Sign Out button displayed)
-        Assert.assertTrue(isAlertPresent());
+        Assert.assertTrue(isElementPresent1(By.xpath("//button[.='Sign Out']")));
     }
-
-
-
 }
