da53e601bb0c250847bf016a736199c5876b0fe7
sustainability99 <126771099+sustainability99@users.noreply.github.com>
2023-07-03 20:51:09 +0200

    added test-results

--- src/main/java/com/phonebook/fw/BaseHelper.java
+++ src/main/java/com/phonebook/fw/BaseHelper.java
@@ -1,9 +1,12 @@
 package com.phonebook.fw;
 
+import com.google.common.io.Files;
 import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.io.File;
+import java.io.IOException;
 import java.time.Duration;
 
 public class BaseHelper {
@@ -60,6 +63,19 @@ public class BaseHelper {
         }
     }
 
+    public String takeScreenshot() {
 
+        File tmp = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+
+        File screenshot = new File("screenshots/screen" + System.currentTimeMillis() + ".png");
+
+        try {
+            Files.copy(tmp,screenshot);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return screenshot.getAbsolutePath();
+    }
 
 }
